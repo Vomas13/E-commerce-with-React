0538bb193252401e8e3ae986e53c520a78196ed9
unknown <Houssamalahmadieh99@Gmail.com>
2023-11-27 06:17:10 +0200

    Created the Home Page UI

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Container,Row,Col} from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+<Row>
+
+        <Col className='text-center py3'>
+            Copy Right &copy; Proshop
+        </Col>
+</Row>
+
+        </Container>
+        </footer>
+  )
+}
+
+export default Footer
